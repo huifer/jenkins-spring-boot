00590913f9b392bdec1d6ddee196448df2f7fe0c
huifer <wangtaolee97@gmail.com>
2019-09-27 21:44:48 +0800

    Initial commit

---
+++ src/main/java/com/huifer/jenkinsspringboot/JenkinsSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.huifer.jenkinsspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsSpringBootApplication.class, args);
+    }
+
+}
