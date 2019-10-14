5c49a9bce2f53e53c6c2b9ce5b4b7c019c0de725
huifer <huifer97@163.com>
2019-10-14 19:16:26 +0800

    修复

--- src/test/java/com/huifer/jenkinsspringboot/JenkinsSpringBootApplicationTests.java
+++ src/test/java/com/huifer/jenkinsspringboot/JenkinsSpringBootApplicationTests.java
@@ -1,6 +1,5 @@
 package com.huifer.jenkinsspringboot;
 
-import com.huifer.jenkinsspringboot.config.SenboRestConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
