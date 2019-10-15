518e2fffd11496b6a664cc0476075cb44578d9e9
huifer <huifer97@163.com>
2019-10-15 09:26:55 +0800

    xz spider

--- src/main/java/com/huifer/jenkinsspringboot/entity/xz/TXz.java
+++ src/main/java/com/huifer/jenkinsspringboot/entity/xz/TXz.java
@@ -1,14 +1,23 @@
 package com.huifer.jenkinsspringboot.entity.xz;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 @Data
+@TableName("t_xz")
 public class TXz {
+    @TableId(value = "id",type = IdType.INPUT)
     private Integer id;
 
+    @TableField("city_id")
     private Integer cityId;
 
+    @TableField("name")
     private String name;
 
+    @TableField("url")
     private String url;
 }
